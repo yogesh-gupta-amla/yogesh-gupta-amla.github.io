b7dc51e5c438c13c191fa1bff5acde10be034bb0
Yogesh Gupta <yogesh.gupta@amla.io>
2025-10-07 17:47:47 +0530

    file changes

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
 // export const APPLICATION_URL = "http://localhost:3000";
-export const APPLICATION_URL = "https://webstore-klrt-dv.amla.io";
+export const APPLICATION_URL = "https://webstore-klrt-np.znodecorp.com";
