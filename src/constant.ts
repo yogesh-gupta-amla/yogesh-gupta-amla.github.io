e899137e3ae98ce693c1dab8c7526eb41614f432
Yogesh Gupta <yogesh.gupta@amla.io>
2025-10-07 18:44:06 +0530

    file changes

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,2 @@
 // export const APPLICATION_URL = "http://localhost:3000";
-export const APPLICATION_URL =
-  "https://cors-anywhere.herokuapp.com/https://webstore-klrt-np.znodecorp.com";
+export const APPLICATION_URL = "https://webstore-klrt-np.znodecorp.com";
